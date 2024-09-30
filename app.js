d6016d2b0e68df6fbc9333d2257c4610fa1c52ac
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-09-30 18:19:03 +0500

    .appened child .remove child

---
+++ app.js
@@ -0,0 +1,45 @@
+// .appened
+// var newBtn = document.createElement("button");
+// newBtn.innerText="click me";
+// console.log(newBtn);
+// var div = document.querySelector("div");
+// div.append(newBtn);
+
+// newBtn.style.background="pink";
+
+
+// .prepend
+// var newBtn = document.createElement("button");
+// newBtn.innerText="click me";
+// console.log(newBtn);
+// var div = document.querySelector("div");
+// div.prepend(newBtn);
+
+// var newBtn = document.createElement("button");
+// newBtn.innerText="click me";
+// console.log(newBtn);
+// var div = document.querySelector("div");
+// div.after(newBtn);
+
+
+// var newBtn = document.createElement("button");
+// newBtn.innerText="click me";
+// console.log(newBtn);
+// var div = document.querySelector("div");
+// div.after(newBtn);
+/////////////////////////////////===================================
+
+// var heading = document.createElement("h1");
+// heading.innerHTML="<i>Hello I am new!</i>"
+// document.querySelector("body").prepend(heading);
+
+// remove();
+// var para = document.querySelector("p");
+// para.remove();
+
+
+// var heading = document.createElement("h1");
+// heading.innerHTML="<i>Hello I am new!</i>"
+// document.querySelector("body").prepend(heading);
+
+// heading.remove();
